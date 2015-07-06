0c453d45f21d2866bea9517b36274dfb5a41eb3c
ZeroZXZ <ZeroZXZ@gmail.com>
2015-07-06 05:45:05 -0300

    Basic HTML Page Setup
    This is a colorful basic html page setup with menu, iframes and some
    images

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+        <meta name="author" content="Sebastião Ribeiro Monteiro Filho">
+        <meta name="keywords" content="html, css, tutorial, exercicio">
+        <meta name="description" content="Exercicio do dia 10/10/2013">
+        <meta http-equiv="refresh" content="120"> <!-- refresh na página, de 2 em 2 minutos -->
+        <base target="#corpoFrame">
+        <title>Bem vindo a Página Principal!</title>
+    </head>
+    <body>
+        <div id="topo">
+            <h1> Tutoriais de Hyper Text Markup Language </h1>
+        </div>
+        
+        <div id="menu">
+
+        </div>
+        <div id="corpo">
+            <iFrame name="corpoFrame" id="corpoFrame" src="principal.html"> </iFrame>
+        </div>
+        <div id="rodape">
+            <iFrame name="rodapeFrame" id="rodapeFrame" src="rodape.html"> </iFrame>
+        </div>
+    </body>
+</html>
